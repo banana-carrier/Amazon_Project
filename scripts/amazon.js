078fc5b022f5eb148cf64c38a02e857ffd3c20ec
banana-boat <admin@bananaboat.local>
2024-10-10 21:01:38 +0200

    Introduced Added message when adding items to the cart

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -45,7 +45,7 @@ products.forEach((product) => {
 
       <div class="product-spacer"></div>
 
-      <div class="added-to-cart">
+      <div class="added-to-cart js-added-to-cart-${product.id}">
         <img src="images/icons/checkmark.png">
         Added
       </div>
@@ -78,5 +78,16 @@ document.querySelectorAll('.js-add-to-cart')
 
       addToCart(productId);
       updateCartQuantity();
+
+      const addedMessage = document.querySelector(
+        `.js-added-to-cart-${productId}`
+      );
+      
+      addedMessage.classList.add('added-to-cart-visible');
+
+      setTimeout(() => {
+        addedMessage.classList.remove('added-to-cart-visible');
+      }, 2000);
+      
     });
   });
